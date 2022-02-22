7f381be48d580c9a8c4f0bfd98c6489801cce72a
Jagat-vasveliya <jk.vasveliya.212@gmail.com>
2022-02-22 10:20:27 +0530

    InProgress

---
+++ src/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="App-header">
+      <div className="heading">
+        <span className="title">Comments</span>
+        <button className="btn">Add</button>
+      </div>
+    </header>
+  );
+}
